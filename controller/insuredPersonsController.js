edf5f79044cf8df35231db88cf03cfe2819ce14a
RashidPanhwar <rashidpanhwer8@gmail.com>
2024-05-05 22:38:26 +0500

    complete crud for insuranced persons

--- controller/insuredPersonsController.js
+++ controller/insuredPersonsController.js
@@ -4,9 +4,47 @@ const catchAsyncError = require('../midleware/catchAsynError');
 exports.addInsuredPerson = catchAsyncError( async(req, res) => {
     const addPerson = await InsuredPersons.create(req.body);
     res.status(201).json({success: true, addPerson})
-} )
+} );
 
 exports.getAllInsuredPersons = catchAsyncError( async(req, res) => {
     const getPersons = await InsuredPersons.find();
     res.status(202).json({success: true, getPersons});
+})
+
+exports.getSingleInsuredPerson = catchAsyncError(async(req, res) => {
+    const {personId} = req.params;
+    const getSinglePerson = await InsuredPersons.findOne({insuredID: personId});
+    res.status(202).json({success: true, getSinglePerson});
+});
+
+exports.deleteInsuredPerson = catchAsyncError(async(req, res) => {
+    const {personId} = req.params;
+    const deletedPerson = await InsuredPersons.findByIdAndDelete({_id: personId});
+    res.status(203).json({success: true, deletedPerson});
+});
+
+exports.updateInsuredPerson = catchAsyncError(async(req, res) => {
+    const {
+        personID, 
+        healthCode, 
+        documentNo, 
+        issueDate, 
+        name, 
+        planCode, 
+        dateOfBirth, 
+        age, 
+        gender, 
+        relation, 
+        cnicNumber, 
+        headCNIC } = req.body;
+
+        const {personId} = req.params;
+        const insuredPerson = await InsuredPersons.findById(personId);
+        if(!insuredPerson){
+            console.log("User Not Found");
+            res.status(404).json({success: false, message: "User Not Found"});
+        }
+        await InsuredPersons.findByIdAndUpdate(personId, req.body);
+        res.status(201).json({success: true, insuredPerson});
+
 })
--- routes/insuredPersonsRoutes.js
+++ routes/insuredPersonsRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const {addInsuredPerson, getAllInsuredPersons} = require('../controller/insuredPersonsController');
+const {addInsuredPerson, getAllInsuredPersons, getSingleInsuredPerson, deleteInsuredPerson, updateInsuredPerson} = require('../controller/insuredPersonsController');
 
 router.post("/addInsuredPerson", addInsuredPerson);
 router.get("/getAllInsuredPersons", getAllInsuredPersons);
+router.get("/getSingleInsuredPerson/:personId", getSingleInsuredPerson);
+router.delete("/deleteInsuredPerson/:personId", deleteInsuredPerson);
+router.put("/updateInsuredPerson/:personId", updateInsuredPerson);
 
 module.exports = router;
